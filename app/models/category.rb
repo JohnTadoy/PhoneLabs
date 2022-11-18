f24cda5358e6e1c8801e730e236abd178752f26c
JohnTadoy <johntadoy1@gmail.com>
2022-11-17 18:48:21 -0600

    Created foreign keys and category table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,4 @@
 class Category < ApplicationRecord
   has_many :products
-  validates :apple, presence: true
-  validates :samsung, presence: true
-  validates :nokia, presence: true
-  validates :redmi, presence: true
-  validates :realmi, presence: true
-  validates :oppo, presence: true
+  validates :name, presence: true
 end

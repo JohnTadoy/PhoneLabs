3c4df6c2f900a3e1859ee9c84493a6b2fbd26f5a
JohnTadoy <johntadoy1@gmail.com>
2022-11-18 11:59:11 -0600

    Added search functionality

--- app/controllers/mobile_phone_controller.rb
+++ app/controllers/mobile_phone_controller.rb
@@ -6,4 +6,15 @@ class MobilePhoneController < ApplicationController
   def show
     @show = Product.find(params[:id])
   end
+
+  def search
+    find = "%#{params[:q]}%"
+    mobile = "%#{params[:type]}%"
+
+    if mobile == ""
+      @mobile = Product.where("name LIKE ?", find)
+    else
+      @mobile = Product.where("name LIKE ? AND category_id LIKE ?", find, mobile)
+    end
+  end
 end
